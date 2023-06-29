6e2755aa9aab01c2ad33438d92cac3e530f25075
Ramiz Qudsi <qudsiramiz@gmail.com>
2023-06-29 13:32:07 -0400

    updating the read function

--- codes/lxi_csv_to_cdf.py
+++ codes/lxi_csv_to_cdf.py
@@ -61,16 +61,16 @@ def lxi_csv_to_cdf(df=None, csv_file=None, csv_folder=None, cdf_file=None, cdf_f
                 csv_file_datetime[xx] = csv_file_secs + csv_file_subsecs / 1e3 + time_ind
             df.index = csv_file_datetime
 
-        # If the cdf_folder does not exist, create it
-        if cdf_folder is not None:
-            if not Path(cdf_folder).exists():
-                Path(cdf_folder).mkdir(parents=True, exist_ok=True)
-                print(f"\n \033[1;32mCreated folder {cdf_folder}\033[0m \n")
-        else:
-            cdf_folder = "/".join(csv_file.split("/")[0:-2]) + "/cdf"
-            if not Path(cdf_folder).exists():
-                Path(cdf_folder).mkdir(parents=True, exist_ok=True)
-                print(f"\n \033[1;32mCreated folder {cdf_folder}\033[0m \n")
+    # If the cdf_folder does not exist, create it
+    if cdf_folder is not None:
+        if not Path(cdf_folder).exists():
+            Path(cdf_folder).mkdir(parents=True, exist_ok=True)
+            print(f"\n \033[1;32mCreated folder {cdf_folder}\033[0m \n")
+    else:
+        cdf_folder = "/".join(csv_file.split("/")[0:-2]) + "/cdf"
+        if not Path(cdf_folder).exists():
+            Path(cdf_folder).mkdir(parents=True, exist_ok=True)
+            print(f"\n \033[1;32mCreated folder {cdf_folder}\033[0m \n")
 
         # If the cdf_file does not exist, create it
         if cdf_file is None:
@@ -83,8 +83,10 @@ def lxi_csv_to_cdf(df=None, csv_file=None, csv_folder=None, cdf_file=None, cdf_f
         if Path(cdf_file).exists():
             # Raise a warning saying the file already exists and ask the user if they want to
             # overwrite it
-            print(f"\n \x1b[1;31;255m WARNING: The CDF file already exists and will be overwritten:"
-                  f" {cdf_file} \x1b[0m")
+            print(
+                f"\n \x1b[1;31;255m WARNING: The CDF file already exists and will be overwritten:"
+                f" {cdf_file} \x1b[0m"
+            )
             Path(cdf_file).unlink()
 
         print(f"Creating CDF file: {cdf_file}")
@@ -106,7 +108,13 @@ def lxi_csv_to_cdf(df=None, csv_file=None, csv_folder=None, cdf_file=None, cdf_f
         df.index = pd.to_datetime(df.index, utc=True, unit="s")
         cdf_data["Epoch"] = df.index
         for col in df.columns:
-            cdf_data[col] = df[col]
+            # If the column is either "utc_time" or "local_time", convert it to a datetime object and
+            # then to a CDF variable
+            if col == "utc_time" or col == "local_time":
+                df[col] = pd.to_datetime(df[col], utc=True)
+                cdf_data[col] = df[col]
+            else:
+                cdf_data[col] = df[col]
         cdf_data.close()
         print(f"\n  CDF file created: \x1b[1;32;255m {cdf_file} \x1b[0m")
 
